ff1948a9931bdf82f13a10153d8278a54240f2ad
dinleo <dinleo11@gmail.com>
2022-01-05 13:38:34 +0900

    Revert "Revert ".idea""
    This reverts commit 23e9bf6a89d0e02934ddd719d575442e4681a020.

--- Go_Code/Nomad_Learngo/Chap2/Dictonary/mydict/mydict.go
+++ Go_Code/Nomad_Learngo/Chap2/Dictonary/mydict/mydict.go
@@ -15,6 +15,7 @@ func (d Dictionary) Search(word string) (int, error) {
 	return 0, errNotFound
 }
 
+// Add num for key
 func (d Dictionary) Add(word string, num int) error {
 	_, err := d.Search(word)
 	if err == errNotFound {
